39c584f25c8a80f8f801e5cdd7b1aa334f3e514e
Alex Jokela <ajokela@umn.edu>
2016-09-14 11:03:35 -0500

    Setting project name and repo location

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,9 +1,9 @@
 # Copyright (c) 2012-2016 Regents of the University of Minnesota
 #
-# This file is part of the Minnesota Population Center's {PROJECT TITLE}.
+# This file is part of the Minnesota Population Center's Terra Populus Project.
 # For copyright and licensing information, see the NOTICE and LICENSE files
 # in this project's top-level directory, and also on-line at:
-#   https://github.com/mnpopcenter/{REPO-NAME}
+#   https://github.com/mnpopcenter/mound
 
 
 Rails.application.routes.draw do
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -1,10 +1,10 @@
 # encoding: UTF-8
 # Copyright (c) 2012-2016 Regents of the University of Minnesota
 #
-# This file is part of the Minnesota Population Center's {PROJECT TITLE}.
+# This file is part of the Minnesota Population Center's Terra Populus Project.
 # For copyright and licensing information, see the NOTICE and LICENSE files
 # in this project's top-level directory, and also on-line at:
-#   https://github.com/mnpopcenter/{REPO-NAME}
+#   https://github.com/mnpopcenter/mound
 
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
